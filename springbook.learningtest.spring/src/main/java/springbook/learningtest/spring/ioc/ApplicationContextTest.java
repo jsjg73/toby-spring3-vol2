d8aea4ee7d2a05ce1385cf6f4f2d6778c40d8b23
jsjg73 <kjs930703@naver.com>
2021-11-16 21:59:23 +0900

    RootBeanDefinition으로 빈 메타정보 등록

--- springbook.learningtest.spring/src/main/java/springbook/learningtest/spring/ioc/ApplicationContextTest.java
+++ springbook.learningtest.spring/src/main/java/springbook/learningtest/spring/ioc/ApplicationContextTest.java
@@ -5,6 +5,8 @@ import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
 
 import org.junit.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.support.RootBeanDefinition;
 import org.springframework.context.support.StaticApplicationContext;
 
 import springbook.learningtest.spring.ioc.bean.Hello;
@@ -18,5 +20,10 @@ public class ApplicationContextTest {
 		
 		Hello hello1 = (Hello) ac.getBean("hello1");
 		assertThat(hello1, is(notNullValue()) );
+		
+		BeanDefinition helloDef = new RootBeanDefinition(Hello.class);
+		helloDef.getPropertyValues().addPropertyValue("name", "Spring");
+		ac.registerBeanDefinition("hello2", helloDef);
+		
 	}
 }
