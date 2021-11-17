b3ab936bffc04eea3f286dba883abbbe72030d8f
jsjg73 <kjs930703@naver.com>
2021-11-17 12:55:26 +0900

    GenericXmlApplicationContext의 학습 테스트
    XmlBeanDefinitionReader를 내장하고 있는 어플리케이션 컨텍스트 구현체

--- springbook.learningtest.spring/src/main/java/springbook/learningtest/spring/ioc/ApplicationContextTest.java
+++ springbook.learningtest.spring/src/main/java/springbook/learningtest/spring/ioc/ApplicationContextTest.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.config.RuntimeBeanReference;
 import org.springframework.beans.factory.support.RootBeanDefinition;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 import org.springframework.context.support.StaticApplicationContext;
 
 import springbook.learningtest.spring.ioc.bean.Hello;
@@ -62,6 +63,17 @@ public class ApplicationContextTest {
 		XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(ac);
 		reader.loadBeanDefinitions("springbook/learningtest/spring/ioc/genericApplicationContext.xml");
 		ac.refresh();
+		
+		Hello hello = ac.getBean("hello", Hello.class);
+		hello.print();
+		
+		assertThat(ac.getBean("printer").toString(), is("Hello Spring"));
+	}
+	
+	@Test
+	public void genericXmlApplicationContext() {
+		GenericXmlApplicationContext ac = new GenericXmlApplicationContext(
+				"springbook/learningtest/spring/ioc/genericApplicationContext.xml");
 		
 		Hello hello = ac.getBean("hello", Hello.class);
 		hello.print();
