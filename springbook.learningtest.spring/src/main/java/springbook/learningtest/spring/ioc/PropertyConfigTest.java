228c09306ec237cfe4f305da439f5aad8dafe750
jsjg73 <kjs930703@naver.com>
2021-12-01 13:32:22 +0900

    valueInjection 컬렉션 학습 테스트

--- springbook.learningtest.spring/src/main/java/springbook/learningtest/spring/ioc/PropertyConfigTest.java
+++ springbook.learningtest.spring/src/main/java/springbook/learningtest/spring/ioc/PropertyConfigTest.java
@@ -3,12 +3,20 @@ package springbook.learningtest.spring.ioc;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
+
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 import org.springframework.core.env.Environment;
@@ -64,13 +72,79 @@ public class PropertyConfigTest {
 		@Value("${database.username}") String username;
 		@Autowired Hello hello;
 	}
-	static class Hello{
-		String username;
-	}
+	static class Hello{ String username; }
 	
 	static class DatabasePropertyPlaceHolder extends PropertyPlaceholderConfigurer{
 		public DatabasePropertyPlaceHolder() {
 			this.setLocation(new ClassPathResource("database.properties", getClass()));
+		}
+	}
+	
+	
+	@Test
+	public void importResource() {
+		AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ConfigA.class);
+		
+		BeanSp bean = ac.getBean(BeanSp.class);
+		
+		assertThat(bean.os, is("Windows 10"));
+		assertThat(bean.username, is("jsjg73"));
+	}
+	@ImportResource("classpath:springbook/learningtest/spring/ioc/propertiesBeanFactoryPostProcessor.xml")
+	@Configuration
+	static class ConfigA{
+		@Bean
+		public BeanSp beanSp() {
+			return new BeanSp();
+		}
+		@Bean
+		public Hello hello() {
+			return new Hello();
+		}
+	}
+	
+	@Test
+	public void collectionInject() {
+		ApplicationContext ac = new GenericXmlApplicationContext(new ClassPathResource("collection.xml", getClass()));
+		BeanC bean = ac.getBean(BeanC.class);
+		
+		assertThat(bean.nameList.size(), is(3));
+		assertThat(bean.nameList.get(0), is("Spring"));
+		assertThat(bean.nameList.get(1), is("IoC"));
+		assertThat(bean.nameList.get(2), is("DI"));
+		
+		assertThat(bean.nameSet.size(), is(3));
+		
+		assertThat(bean.ages.get("Kim"), is(30));
+		assertThat(bean.ages.get("Lee"), is(35));
+		assertThat(bean.ages.get("Ahn"), is(40));
+		
+		assertThat((String)bean.settings.get("username"), is("Spring"));
+		assertThat((String)bean.settings.get("password"), is("Book"));
+		
+		assertThat(bean.beans.size(), is(2));
+	}
+	
+	static class BeanC{
+		List<String> nameList;
+		Set<String> nameSet;
+		Map<String, Integer> ages;
+		Properties settings;
+		List beans;
+		public void setNameList(List<String> nameList) {
+			this.nameList = nameList;
+		}
+		public void setNameSet(Set<String> nameSet) {
+			this.nameSet = nameSet;
+		}
+		public void setAges(Map<String, Integer> ages) {
+			this.ages = ages;
+		}
+		public void setSettings(Properties settings) {
+			this.settings = settings;
+		}
+		public void setBeans(List beans) {
+			this.beans = beans;
 		}
 	}
 }
