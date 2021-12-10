61f66130ae038485f232afa06b4cbef008064c5b
jsjg73 <kjs930703@naver.com>
2021-12-10 22:26:49 +0900

    session 스코프 빈 학습테스트: 프록시 사용하도록 수정

--- springbook.learningtest.spring/src/main/java/springbook/learningtest/spring/ioc/scope/session/SessionScopeTest.java
+++ springbook.learningtest.spring/src/main/java/springbook/learningtest/spring/ioc/scope/session/SessionScopeTest.java
@@ -9,29 +9,38 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 
 public class SessionScopeTest {
 	
 	
 	static class LoginService {
-		@Autowired Provider<LoginUser> provider;
+//		@Autowired Provider<LoginUser> provider;
+		@Autowired LoginUser loginUser;
+		
 		public LoginUser getUser() {
-			return provider.get();
+			return loginUser;
 		}
 	}
-
-	@Scope("session")
+	
+	//클래스 모드 스코프 프록시 설정
+	@Scope(value= "session", proxyMode = ScopedProxyMode.TARGET_CLASS)
 	static class LoginUser {
 		String loginId;
 		String name;
 		Date loginTime;
+		
+		public void setLoginId(String loginId) {
+			this.loginId = loginId;
+		}
 	}
 
-	@Test(expected = IllegalStateException.class)
+//	@Test(expected = IllegalStateException.class)
+	@Test
 	public void SessionScopeBeanDLbyProvider() {
 		ApplicationContext ac = new AnnotationConfigApplicationContext(LoginService.class, LoginUser.class);
 		
 		LoginService service = ac.getBean(LoginService.class);
-		LoginUser user = service.getUser();
+		LoginUser user = ac.getBean(LoginUser.class);
 	}
 }
