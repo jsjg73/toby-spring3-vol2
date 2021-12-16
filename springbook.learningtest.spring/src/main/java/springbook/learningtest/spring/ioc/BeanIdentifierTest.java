61adcb98672988e086418d4759f0bdb7c5065c5d
jsjg73 <kjs930703@naver.com>
2021-12-16 14:56:20 +0900

    학습테스트/애노테이션에서 빈이름 설정

--- springbook.learningtest.spring/src/main/java/springbook/learningtest/spring/ioc/BeanIdentifierTest.java
+++ springbook.learningtest.spring/src/main/java/springbook/learningtest/spring/ioc/BeanIdentifierTest.java
@@ -1,15 +1,23 @@
 package springbook.learningtest.spring.ioc;
 
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.CoreMatchers.sameInstance;
 import static org.junit.Assert.assertThat;
 
 import java.util.Arrays;
 
+import org.hamcrest.core.IsInstanceOf;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.GenericXmlApplicationContext;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Component;
 
 import springbook.learningtest.spring.ioc.bean.Hello;
 
@@ -31,5 +39,25 @@ public class BeanIdentifierTest {
 		assertThat(ac.getBean("hello_alias",Hello.class), notNullValue());
 	}
 	
+	@Test
+	public void byJavaCode() {
+		ApplicationContext ac = new AnnotationConfigApplicationContext(Config.class, BeanA.class);
+		BeanA method = ac.getBean("method",BeanA.class);
+		BeanA component = ac.getBean("component",BeanA.class);
+		assertThat(method, not(sameInstance(component)) );
+	}
 	
+	
+	@Configuration
+	static class Config{
+		@Bean(name="method")
+		public BeanA beanA() {
+			return new BeanA("method");
+		}
+	}
+	
+	@Component("component")
+	static class BeanA{String nm="component"; 
+	public BeanA() {}
+	public BeanA(String nm) {this.nm=nm;}	}
 }
