16f99423a87a981c677c15633cb4cc4d5fd121a0
jsjg73 <kjs930703@naver.com>
2021-12-20 00:37:06 +0900

    학습테스트/ibatis/내장db

---
+++ springbook.learningtest.spring/src/main/java/springbook/learningtest/spring/ibatis/IbatisTest.java
@@ -0,0 +1,93 @@
+package springbook.learningtest.spring.ibatis;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.orm.ibatis.SqlMapClientFactoryBean;
+import org.springframework.orm.ibatis.SqlMapClientTemplate;
+
+import com.ibatis.sqlmap.client.SqlMapClient;
+
+import springbook.learningtest.spring.embeddeddb.MemberEmbeddedDB;
+import springbook.learningtest.spring.jdbc.Member;
+
+public class IbatisTest {
+//	private EmbeddedDatabase db;
+	
+	@Test
+	public void ibatis() {
+		ApplicationContext ac = new AnnotationConfigApplicationContext(IbatisConfig.class);
+		IbatisDao dao = ac.getBean(IbatisDao.class);
+		
+//		dao.deleteAll();
+		dao.add(new Member(1,"jsjg73", 1.5));
+		Member m = dao.select(1);
+		assertThat(m.getId(), is(1));
+		assertThat(m.getName(), is("jsjg73"));
+		assertTrue(m.getPoint()==1.5);
+		List<Member> list = dao.selectAll();
+		for(Member mm : list) {
+			System.out.println(mm.getPoint());
+		}
+	}
+	
+	public static class IbatisDao{
+		private SqlMapClientTemplate sqlMapClientTemplate;
+		
+		public void setSqlMapClient(SqlMapClient sqlMapClient) {
+			this.sqlMapClientTemplate = new SqlMapClientTemplate(sqlMapClient);
+		}
+		public List<Member> selectAll() { return sqlMapClientTemplate.queryForList("findMembers"); }
+		public Member select(int i) {
+			return (Member)this.sqlMapClientTemplate.queryForObject("findMemberById", i);
+			
+		}
+		public void deleteAll() {
+			sqlMapClientTemplate.delete("deleteMemberAll");
+		}
+		public void add(Member m) {
+			sqlMapClientTemplate.insert("insertMember", m);
+		}
+	}
+	
+	@Configuration
+	public static class IbatisConfig{
+		@Bean
+		public IbatisDao ibatisDao(){
+			IbatisDao dao = new IbatisDao();
+			dao.setSqlMapClient(sqlMapClient());
+			return dao;
+		}
+
+		@Bean
+		public SqlMapClient sqlMapClient() {
+			SqlMapClientFactoryBean factory = new SqlMapClientFactoryBean();
+			factory.setDataSource(embeddedDB());
+			factory.setConfigLocation(new ClassPathResource("SqlMapConfig.xml", getClass()));
+			try {
+				factory.afterPropertiesSet();
+			} catch (Exception e) {
+				e.printStackTrace();
+				throw new RuntimeException();
+			}
+			SqlMapClient smc =(SqlMapClient)factory.getObject();
+			System.out.println(smc);
+			return smc;
+		}
+		
+		public EmbeddedDatabase embeddedDB() {
+			return MemberEmbeddedDB.getEmbeddedDatabase();
+		}
+	}
+}
